e0ff81060956627bb8f1d639bc7b91d5f1ce2006
AnjaliMusmade <anjalimusmade29@gmail.com>
2019-12-02 10:09:16 +0530

    2 files modified

--- actions/actionCreator.tsx
+++ actions/actionCreator.tsx
@@ -4,3 +4,25 @@ export function addToDo(text: string) {
     text: text
   };
 }
+
+export function TOGGLE_TODO(text: string) {
+  return {
+    type: "TOGGLE_TODO",
+    text: text
+  };
+}
+
+export function SET_FILTER(text: string) {
+  return {
+    type: "SET_FILTER",
+    text: text
+  };
+}
+
+export function SET_COLOR(color: string) {
+  return {
+    type: "SET_COLOR",
+    text: color
+  };
+}
+
--- index.tsx
+++ index.tsx
@@ -9,7 +9,10 @@ import {createStore,combineReducers} from 'redux';
 import todosReducer from './reducers/todoReducer';
 import filterReducer from './reducers/filterReducer';
 import colorReducer from './reducers/colorReducer';
-import {addToDo} from './actions/actionCreator'
+import {addToDo} from './actions/actionCreator';
+import {toggleToDO} from './actions/actionCreator';
+import {setFilter} from './actions/actionCreator';
+import {setColor} from './actions/actionCreator';
 
 
 let store=createStore(combineReducers({todos: todosReducer,filter: filterReducer, color:colorReducer}));
@@ -17,8 +20,8 @@ let store=createStore(combineReducers({todos: todosReducer,filter: filterReducer
 store.subscribe(()=>console.log(console.log(store.getState())));
 
 store.dispatch(addToDo("Onion"));
-store.dispatch({type:"ADD_TODO",text:"Pen"});
- store.dispatch({type:"ADD_TODO",text:"Milk"});
- store.dispatch({type:"TOGGLE_TODO",text:"Onion"});
- store.dispatch({type:"SET_FILTER",visibility:"SHOW_COMPLETED"});
- store.dispatch({type:"SET_COLOR",color:"blue"});
+store.dispatch(addToDo("Pen"));
+ store.dispatch(addToDo("Milk"));
+ store.dispatch(toggleToDO("Onion"));
+ store.dispatch(setFilter("SHOW_COMPLETED"));
+ store.dispatch(setColor("blue"));
